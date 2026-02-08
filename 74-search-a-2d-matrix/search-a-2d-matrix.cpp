f587cdd243fb7ee3d6f63fee116d10736482c899
Kpanicke21 <kpanicke@asu.edu>
2026-02-07 18:18:27 -0700

    Time: 0 ms (100.00%) | Memory: 13.3 MB (77.26%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        int left = 0;
+        int right = rows * cols - 1;
+        int firstTrueIndex = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int row = mid / cols;
+            int col = mid % cols;
+
+            if (matrix[row][col] >= target) {
+                firstTrueIndex = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        if (firstTrueIndex == -1) {
+            return false;
+        }
+        int row = firstTrueIndex / cols;
+        int col = firstTrueIndex % cols;
+        return matrix[row][col] == target;
+    }
+};
